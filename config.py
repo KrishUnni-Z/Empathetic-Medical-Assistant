098c72030008c4d7dbe5367f50e97bb6370fdb42
Unni <krishnan.u.prasad@student.uts.edu.au>
2025-04-20 20:05:24 +1000

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,11 @@ import streamlit as st
 from openai import AsyncOpenAI
 from agents import OpenAIChatCompletionsModel, ModelProvider, Model
 
-# Setup GPT client using secrets
 client = AsyncOpenAI(
     base_url=st.secrets["AZURE_OPENAI_ENDPOINT"],
     api_key=st.secrets["AZURE_OPENAI_API_KEY"],
 )
 
-# Model Provider
 class EmpatheticModelProvider(ModelProvider):
     def get_model(self, model_name) -> Model:
         return OpenAIChatCompletionsModel(model=model_name, openai_client=client)
