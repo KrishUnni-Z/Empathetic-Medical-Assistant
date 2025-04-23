87309a64a1db4046194a93fe3920af8e7fdebe80
Unni <krishnan.u.prasad@student.uts.edu.au>
2025-04-23 11:13:41 +1000

    Update core.py

--- core.py
+++ core.py
@@ -1,3 +1,4 @@
+# core.py
 import os
 import requests
 from datetime import datetime
@@ -5,42 +6,32 @@ from transformers import pipeline
 
 emotion_classifier = pipeline("text-classification", model="bhadresh-savani/bert-base-go-emotion", top_k=1)
 
-user_profile = {"name": "", "age": "", "gender": "", "location": ""}
-context_info = {"time": "", "location": "", "emotion": ""}
-
 profile_path = "user_profile.txt"
 emotion_log_path = "emotion_log.txt"
+
+user_profile = {"name": "", "age": "", "gender": ""}
+context_info = {"time": "", "location": "", "emotion": ""}
 
 def load_profile():
     if os.path.exists(profile_path):
         with open(profile_path, "r") as f:
             lines = f.read().splitlines()
-            keys = list(user_profile.keys())
-            for i in range(min(len(lines), len(keys))):
-                user_profile[keys[i]] = lines[i]
+            if len(lines) >= 3:
+                user_profile["name"], user_profile["age"], user_profile["gender"] = lines[:3]
 
 def save_profile():
     with open(profile_path, "w") as f:
-        for v in user_profile.values():
-            f.write(v + "\n")
+        f.write(f"{user_profile['name']}\n{user_profile['age']}\n{user_profile['gender']}")
+
+def get_time():
+    return datetime.now().strftime("%A, %I:%M %p")
 
 def get_location():
     try:
         res = requests.get("https://ipinfo.io/json").json()
         return res.get("city", "") + ", " + res.get("region", "")
     except:
-        return "Unknown"
-
-def get_time(city="UTC"):
-    try:
-        res = requests.get(f"https://worldtimeapi.org/api/timezone")
-        timezones = res.json()
-        guess = next((tz for tz in timezones if city.lower() in tz.lower()), "Etc/UTC")
-        time_data = requests.get(f"https://worldtimeapi.org/api/timezone/{guess}").json()
-        dt = datetime.fromisoformat(time_data["datetime"].split("+")[0])
-        return dt.strftime("%A, %I:%M %p")
-    except:
-        return datetime.now().strftime("%A, %I:%M %p")
+        return "Unknown location"
 
 def get_emotion(text):
     try:
